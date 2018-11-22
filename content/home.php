a20e041909e0b0e7b3d7e11c8d83344697c7124c
unknown <ms2301@gmx.de>
2018-11-22 23:35:03 +0100

    [offenburg] update event data
    - update pause information
    - update press stuff

--- content/home.php
+++ content/home.php
@@ -1,8 +1,8 @@
 <h1>Nontacet - Percussion Ensemble</h1>
 <?php
 
-$refDate=new DateTime("2018-03-11 23:30:00");
-$nowDate=new DateTime('NOW');
+$refDate=new DateTime("2018-03-11 23:30:00", new DateTimeZone('Europe/Berlin'));
+$nowDate=new DateTime('NOW', new DateTimeZone('Europe/Berlin'));
 
 if($nowDate < $refDate)
     {
@@ -17,9 +17,34 @@ if($nowDate < $refDate)
 <p>
 <img id="TitleImage"
      
-     src="./images/title.jpg" 
+     src="./images/title_pause.jpg" 
      alt="Nontacet Titelbild"/>
 </p>
+
+<h3 class="bold">
+Ab sofort werden wir vorerst keine neuen Anfragen mehr annehmen
+</h3>
+
+<p>Hintergrund dieser Entscheidung:</p>
+<p>Wir spielen in dieser Besetzung schon seit einigen Jahren zusammen und das mit Überzeugung und viel Spass.
+In dieser Zeit haben sich sowohl unsere privaten als auch beruflichen Umstände verändert.</p>
+<p>Dies wirkt sich auf die verfügbare Zeit für Üben, Proben und Auftritte aus.
+</p>
+<p>Wenn wir für euch spielen, wollen wir mit der Leistung zufrieden sein und vor allem dass ihr gut unterhalten seid. Mit der Phase bis Januar 2019 haben wir wieder ein neues Programm erarbeitet, um das Repertoir zu erneuern. Das können wir so in absehbarer Zukunft leider nicht mehr leisten. 
+</p>
+<p></p>
+<p>Aus diesen Gründen haben wir uns dazu entschieden, vorerst keine Termine mehr anzunehmen und uns musikalisch für eine Ruhepause zurück zu ziehen.
+</p>
+
+<p>Anfragen können nach wie vor gestellt werden, allerdings mit der Aussicht auf eine Absage.</p>
+<p></p>
+
+
+
+
+
+    
+    
 <article style="<?php echo $beforeStyle;  ?>">
     <h3>Neue Termine</h3>
     <p>
--- content/press.php
+++ content/press.php
@@ -1,7 +1,16 @@
+<p>
+    "Dem Bollschweiler Percussion- Ensemble Nontacet gelingt es immer wieder aufs Neue sein Publikum zu überraschen."
+</p>
+<p>Quelle: Badische Zeitung, 20. Juli 2018<br>
+    Link: <a target="_blank"
+              href="http://www.badische-zeitung.de/bollschweil/schlag-auf-schlag-x7x">Schlag auf Schlag</a>
+</p>
+
+
 <p>"... Sie schlagen auf Backen, Brust, Beine oder Bongos, mit der flachen Hand, den Fingerspitzen oder der geballten Faust. Sie agieren absolut synchron, zeigen eine perfekt abgestimmte Choreografie, tanzen mit ihren Händen über die und auf den Instrumenten..."</p>
 <p>
-    Quelle: 
-    <a href="mehr.bz/nontacet"
+    Quelle: Badische Zeitung, 19. März 2018<br>
+    Link: <a href="http://www.mehr.bz/nontacet"
        target="_blank">Im Schlagen unschlagbar</a>
     
 </p>
@@ -15,10 +24,9 @@
 </a>
 </p>
 <p>"Waschechte "Eigengewächse" sind die Vier von der Gruppe "Non Tacet" mit ihrem Programm "Percussion Night", die sich am 25. Juni die Ehre geben werden: Marcel Hug, Christoph Steiert, Lucas Grammelspacher und Markus Schneider, als Schlagzeuger in den Trachtenkapellen Bollschweil und St. Ulrich aufgewachsen. Inzwischen haben sie ihren eigenen Stil entwickelt und begeistern mit origineller Handhabung der verschiedensten Rhythmusinstrumente, kommen aber gelegentlich auch ganz ohne aus."<br>
-Quelle: 
-<a href="http://www.badische-zeitung.de/bollschweil/kleinkuenstler-haben-bei-bolando-eine-buehne--102219148.html"
-   target="_blank">
-http://www.badische-zeitung.de/bollschweil/kleinkuenstler-haben-bei-bolando-eine-buehne--102219148.html
+Quelle: Badische Zeitung, 23. März 2015<br>
+Link: <a href="http://www.badische-zeitung.de/bollschweil/kleinkuenstler-haben-bei-bolando-eine-buehne--102219148.html"
+   target="_blank">Kleinkünstler haben bei Bolando eine Bühne
 </a>
 
 </p>
@@ -26,10 +34,9 @@ http://www.badische-zeitung.de/bollschweil/kleinkuenstler-haben-bei-bolando-eine
 <p>
 "...der Auftritt von "Non tacet", der Percussion Band von Bollschweil, bereicherten das Programm"
 <br>
-Quelle: 
-<a href="http://www.badische-zeitung.de/schallstadt/von-moritat-bis-marimba--80411795.html"
-   target="_blank">
-    http://www.badische-zeitung.de/schallstadt/von-moritat-bis-marimba--80411795.html
+Quelle: Badische Zeitung, 05. Februar 2014<br>
+Link: <a href="http://www.badische-zeitung.de/schallstadt/von-moritat-bis-marimba--80411795.html"
+   target="_blank">Von Moritat bis Marimba
 </a>
     
 
@@ -38,11 +45,9 @@ Quelle:
 <p>
 "Was die fünf Schlagzeuger aus dem Bollschweiler Musikverein an rhythmischer Trommelei auf die Bühne brachten, war wirklich hörens- und sehenswert."
 <br>
-Quelle: <a href="http://www.badische-zeitung.de/bollschweil/ein-mix-aus-vielen-epochen--65573350.html"
-   target="_blank">
-       http://www.badische-zeitung.de/bollschweil/ein-mix-aus-vielen-epochen--65573350.html
+Quelle: Badische Zeitung, 13. November 2012<br>
+Link: <a href="http://www.badische-zeitung.de/bollschweil/ein-mix-aus-vielen-epochen--65573350.html"
+   target="_blank">Ein Mix aus vielen Epochen
 </a>
    
-
-    
 </p>
